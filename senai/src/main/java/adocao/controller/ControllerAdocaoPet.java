e2ebd67384d8db104c881675f1f70202fb40c4ac
Hugy0707 <99295735+Hugy0707@users.noreply.github.com>
2025-12-03 21:50:14 -0300

    ativdade

--- senai/src/main/java/adocao/controller/ControllerAdocaoPet.java
+++ senai/src/main/java/adocao/controller/ControllerAdocaoPet.java
@@ -1,5 +1,18 @@
 package adocao.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin
+@RestController("/api/adocoes")
+@RequestMapping
 public class ControllerAdocaoPet {
+
+    @PostMapping
+
+    @GetMapping
 
 }
