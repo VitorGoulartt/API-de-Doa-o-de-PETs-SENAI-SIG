bc54dd500788e2e4e9cf4b67c2fbb6e4dcc25408
Vitor hugo <99295735+VitorGoulartt@users.noreply.github.com>
2026-01-16 04:08:32 +0000

    oi

--- senai/src/main/java/adocao/config/SecurityConfig.java
+++ senai/src/main/java/adocao/config/SecurityConfig.java
@@ -6,13 +6,23 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity 
 public class SecurityConfig {
+
+    //@Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http)throws Exception {
+        return http.build();
+
+    }
+
 
     //@Bean
     //public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -38,6 +48,15 @@ public class SecurityConfig {
             
    // return http.build();
 //}
+
+    @Bean
+    public InMemoryUserDetailsManager usuarioServico(PasswordEncoder encoder){
+        UserDetails user =  User.withUsername("vitor")
+        .password(encoder.encode("senai123"))
+        .roles("USER")
+        .build();
+        return new  InMemoryUserDetailsManager(user);
+    }
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
